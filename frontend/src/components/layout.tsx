dde383f1d44b38eed29138a6a0559607060e0e47
EJ Mercado <ej@ejmercado.com>
2020-01-28 03:08:34 +0800

    Init home/sign up page add semantic ui react

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Container } from 'semantic-ui-react';
+import { Header } from './header';
 
 interface Props {
     children: JSX.Element | JSX.Element[];
 }
 
 export const Layout: React.FC<Props> = ({ children }) => {
-    return (<Container>
-        {children}
-    </Container>)
+    return (
+        <div>
+            <Header />
+            <Container>
+                {children}
+            </Container>
+        </div>
+    )
 }
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import { Container, Button, Icon, Grid, Form, Divider, Checkbox, Segment, Header, } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 export const Home: React.FC<{}> = () => {
     return (
-        <p>Home</p>
+        <Container text>
+            <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+                <Grid.Column style={{ maxWidth: 450 }}>
+                    <Link to="/">
+                        <Header as="h1">LOGO!</Header>
+                    </Link>
+                    <Segment basic>
+                            <Segment style={{ padding: 0, margin: 0}} basic>Turn your fans into VIP members</Segment>
+                            <Segment style={{ padding: 0, margin: 0}} basic>Interact with them</Segment>
+                            <Segment style={{ padding: 0, margin: 0}} basic>Make money!</Segment>
+                    </Segment>
+                    <Button color='blue' fluid size='big'>
+                        <Grid>
+                            <Grid.Column verticalAlign="middle" textAlign="right" width="11">Continue with Twitter</Grid.Column>
+                            <Grid.Column verticalAlign="middle" textAlign="right" width="5" floated="right"><Icon size="large" name="twitter"></Icon></Grid.Column>
+                        </Grid>
+                    </Button>
+                    <Divider horizontal>OR</Divider>
+                    <Form size='large'>
+                        <Form.Input fluid placeholder='Email' />
+                        <Form.Input
+                            fluid
+                            placeholder='Password'
+                            type='password'
+                        />
+                        <Form.Input
+                            fluid
+                            placeholder='Name'
+                            type='text'
+                        />
+                        <Segment textAlign="center" basic>
+                            <Checkbox label="I agree to the " /> <Link to="#">Terms of Use and Privacy Policy</Link>
+                        </Segment>
+                        <Button color='blue' fluid size='big'>
+                            Sign-up
+                        </Button>
+                    </Form>
+                    <Segment textAlign="center" basic>
+                        Already joined? <a href='/sign-in'>Click here!</a>
+                    </Segment>
+                </Grid.Column>
+            </Grid>
+        </Container>
     )
 }
